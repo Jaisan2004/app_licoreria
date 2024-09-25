13b61c0c41abbadd5ab11a3b14df09e82d70584f
Santyrc0325 <scamachor@uniempresarial.edu.co>
2024-09-25 11:34:29 -0500

    x

--- backend_distribuidora_licores/src/main/java/co/edu/ue/config/SecurityConfig.java
+++ backend_distribuidora_licores/src/main/java/co/edu/ue/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-package co.edu.ue.config;
+// package co.edu.ue.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 	
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-		return http
-				.authorizeHttpRequests(authRequest ->
-				authRequest
-				.requestMatchers("/auth/**").permitAll()
-				.anyRequest().authenticated()
-				).formLogin(withDefaults())
-				.build();
-	}
+// 	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+// 		return http
+// 				.authorizeHttpRequests(authRequest ->
+// 				authRequest
+// 				.requestMatchers("/auth/**").permitAll()
+// 				.anyRequest().authenticated()
+// 				).formLogin(withDefaults())
+// 				.build();
+// 	}
 
-}
+// }
